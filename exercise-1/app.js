9cb1108c03b337956a511a07d6ea73e9a16df65e
diego ramirez <diegomejiasobsu@gmail.com>
2024-04-17 16:35:50 -0500

    commit: exercise 6 declaramos variable miBooleano e imprimimos su typeof

--- exercise-1/app.js
+++ exercise-1/app.js
@@ -1,3 +1,5 @@
 // en los siguientes ejercicios nos familiarizaremos con las variables y los tipos de datos en JS
 let miNumero = 10
 console.log(miNumero)
+let miBooleano = true
+console.log(typeof miBooleano)
